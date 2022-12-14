5c030986bc5ccf3eacc2af2b41fb324f33061842
Mountain96 <jtlsan@naver.com>
2022-12-14 04:25:02 +0000

    Implement validity check for annotation files

--- utils/iaa.py
+++ utils/iaa.py
@@ -10,5 +10,26 @@ class IAA:
         self.workers = len(xls_list)
         self.test_annot_validity(target_category)
 
-    def test_annot_validity(self, target_category):
-        pass
+    def test_annot_validity(self):
+        validity = True
+        error_msg = ''
+        for category in self.categories:
+            valid_check = np.array([])
+            for annot in self.annot_list:
+                sheetname = self.get_sheetname(annot.keys(), category)
+                valid_values = annot[sheetname]['valid_check'].values
+                if valid_check.size == 0:
+                    valid_check = valid_values.copy()
+                else:
+                    if np.array_equal(valid_check, valid_values) is False:
+                        validity = False
+                        indices = np.where((valid_check == valid_values) == False)
+                        error_msg += f'{category}에서 valid_error가 서로 다른 항목이 존재합니다. -> {indices}\n'
+        if validity is False:
+            raise Exception(error_msg)
+            
+    def get_sheetname(self, sheets, category):
+        for sheet in sheets:
+            if category in sheet:
+                return sheet
+        raise Exception(f'{category}와 매칭되는 sheet가 없습니다.')
