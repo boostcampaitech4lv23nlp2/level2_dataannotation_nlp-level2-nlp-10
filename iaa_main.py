0b73c68960c6f4dc515620b83747efc47470bad2
Mountain96 <jtlsan@naver.com>
2022-12-14 16:27:17 +0000

    Update remove_list for now

--- iaa_main.py
+++ iaa_main.py
@@ -4,28 +4,35 @@ import sys
 
 def main(xls_path):
     available_categories = [
-        '제1차세계대전',
-        '유엔',
-        '강대국',
-        '냉전',
-        '중국',
-        '중립국',
-        '공산주의',
-        '연합국',
-        '자유주의',
-        '유대인',
-        '핵무기'
+    '제1차세계대전',
+    '유엔',
+    '강대국',
+    '냉전',
+    '중국',
+    '중립국',
+    '공산주의',
+    '연합국',
+    '자유주의',
+    '유대인',
+    '핵무기'
     ]
     remove_list = [
-        '핵무기', 
-        '냉전',
+        # 라벨링이 아직 안됨
+        '제1차세계대전',
+        '공산주의',
         '중국',
-        '중립국',
-        '연합국'
+        '핵무기',
+        # 기타 오류
+        '유대인',
+        '냉전',
+        '유엔', 
     ]
     for item in remove_list:
         available_categories.remove(item)
-
+    print('\n' + '-'*10 + 'categories' + '-'*10)
+    for category in available_categories:
+        print(category)
+    print('-'*30 + '\n')
     iaa = IAA(target_category=available_categories)
     iaa.create_iaa_xlsx()
     print_fleisskappa('/opt/ml/level2_dataannotation_nlp-level2-nlp-10/data/workers_annot.xlsx')
